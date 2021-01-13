1a7d5020191f504c9149fa9d133cc70a36c1a084
Mardonis Alves Bezerra <mardonisgp@gmail.com>
2021-01-13 11:36:50 -0300

    add-list

--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux'
 import Dashboardreducer from '../dashboard/dashboardReducer'
 import TabReducer from '../common/tab/tabReducers'
+import BillingCycleReducer from '../billingCycle/billingCycleReducer'
 
 const rootReducers = combineReducers({
     dashboard: Dashboardreducer,
-    tab: TabReducer
+    tab: TabReducer,
+    billingCycle: BillingCycleReducer
 })
 
 export default rootReducers
