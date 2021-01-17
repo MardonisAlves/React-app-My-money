5ef0a73a44b5a93580508f25fa2b01c6e953ae80
Mardonis Alves Bezerra <mardonisgp@gmail.com>
2021-01-17 19:54:27 -0300

    add-button-update

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,9 +1,10 @@
 import axios from 'axios'
 import {toastr} from 'react-redux-toastr'
-import { reset as resetForm } from 'redux-form'
+import { reset as resetForm ,initialize } from 'redux-form'
 import {showTabs , selectTab } from '../common/tab/tabActions'
 
 const BASE_URL = 'http://localhost:3003/api'
+const INITIAL_VALUES = {}
 
 
 
@@ -16,18 +17,25 @@ export function getList(){
     }
 }
 
-export function create(values) {
+
+
+export function create(values){
+    return submit(values , 'post')
+}
+export function update(values){
+    return submit(values , 'put')
+}
+
+
+
+function submit(values , method) {
     return dispatch => {
 
-        axios.post(`${BASE_URL}/billingCycles`, values)
+        const id = values._id ? values._id : ''
+        axios[method](`${BASE_URL}/billingCycles/${id}`, values)
         .then(resp => {
             toastr.success('Sucesso' , 'Operação Realizada com sucesso')
-            dispatch([
-                resetForm('billingCycleForm'),
-                getList(),
-                selectTab('tabList'),
-                showTabs('tabList' , 'tabCreate')
-            ])
+            dispatch(init())
         })
         .catch(e =>  {
             e.response.data.errors.forEach(error => toastr.error('Error' , error))
@@ -36,9 +44,22 @@ export function create(values) {
     }
 }
 
+
+
 export function showUpdate(billingCycle){
     return [
         showTabs('tabUpdate'),
-        selectTab('tabUpdate')
+        selectTab('tabUpdate'),
+        initialize('billingCycleForm' , billingCycle)
+    ]
+}
+
+
+export function init(){
+    return [
+        showTabs('tabList', 'tabCreate'),
+        selectTab('tabList'),
+        getList(),
+        initialize('billingCycleForm') , INITIAL_VALUES
     ]
 }
